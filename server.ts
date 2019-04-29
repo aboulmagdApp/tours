6a8e8525863af12e700408008f239e063dd2b765
aboulmagdApp <aboulmagd@live.com>
2019-04-29 17:38:07 +0300

    add mark to tours post

--- server.ts
+++ server.ts
@@ -9,6 +9,6 @@ app.get("/tours",(req,res,next)=>{
     res.send("get List of tours API.....");
 });
 app.post("/tours",(req,res,next)=>{
-    res.send("Add new Tour API.....");
+    res.send("Add new Tour API.....!");
 });
 app.listen(process.env.PORT || 4038,()=>{console.log("Server Started.......")});
