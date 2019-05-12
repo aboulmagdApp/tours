36f92f00c6d7ead45041d944f5abd249b9d7e305
aboulmagdApp <aboulmagd@live.com>
2019-05-12 15:02:57 +0300

    add bookings with paramter using 4 digit

--- server.ts
+++ server.ts
@@ -15,19 +15,32 @@ import morgan from "morgan";
 import path from "path";
 import { apiUploadImage } from "./api/tours/apiUploadImage";
 import { apiErrorHandler } from "./api/general/errorHandling";
+import { runInNewContext } from "vm";
+import { APIError } from "./api/model/shared/messages";
 
 const logger = morgan("dev");
 app.use(logger);
-app.use("/static",express.static(path.resolve("./","pubilc","img")));
+app.use((req, res, next) => {
+    if (req.accepts("application/json")) {
+        next();
+    }
+    else
+    {
+        next(new APIError("content Type not Supported","this API only Supports applicaton/json",400));
+    }
+});
+app.get("/headers", (req, res, next) => res.json(req.headers));
+app.use("/static", express.static(path.resolve("./", "pubilc", "img")));
 app.get("/", (req, res, next) => {
     res.send("Tour Booking API.....");
 });
+app.get("/bookings/:id(\\d{4})",(req,res,next)=>res.json(req.params));
 app.get("/tours", apiGetTours);
 app.get("/tours/:id", apiGetTourDetail);
 app.post("/tours", jsonParser, apiCreateTour);
 app.delete("/tours/:id", apiDeletTour);
 app.patch("/tours/:id", jsonParser, apiUpdateTour);
-app.post("/tours/:id/img",apiUploadImage);
+app.post("/tours/:id/img", apiUploadImage);
 
 app.use(apiErrorHandler);
-app.listen(process.env.PORT || 5039, () => { console.log("Server Started.......")});
+app.listen(process.env.PORT || 5039, () => { console.log("Server Started.......") });
