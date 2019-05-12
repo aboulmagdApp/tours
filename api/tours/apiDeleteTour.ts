1c68243ec27ea41edee47e072364cc66c6bc8be9
aboulmagdApp <aboulmagd@live.com>
2019-05-13 01:50:03 +0300

    change error message with new method

--- api/tours/apiDeleteTour.ts
+++ api/tours/apiDeleteTour.ts
@@ -7,8 +7,8 @@ const tourID = req.params.id;
 const tourIndex = DataStore.tours.findIndex((item:any) => item.id = tourID);
 if(tourIndex > -1){
     DataStore.tours.splice(tourIndex,1);
-    res.json(new PublicInfo("Tour deleted",200));
+    res.json(PublicInfo.infoDeleted());
 }else{
-    next(new APIError("Validation Error","Tour Not Found",400));
+    next(APIError.errNotFound());
 }
 }
