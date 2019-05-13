346472c986894a10fea9fa965e2870f2a2f6e724
aboulmagdApp <aboulmagd@live.com>
2019-05-13 16:22:36 +0300

    Route and clean server file

--- server.ts
+++ server.ts
@@ -1,26 +1,7 @@
 import express from "express";
 const app = express();
-import path from "path";
-import { apiErrorHandler } from "./api/general/errorHandling";
-import { apiDownloadImage } from "./api/tours/apiDownloadImage";
-import { userRouter } from "./api/users/apiUsers";
-import { toursRouter } from "./api/tours/apiTours";
-import { apiCors } from "./api/general/coes";
-import { logger } from "./api/general/logging";
-import { apiValidation } from "./api/general/validation";
+import { routerV1 } from "./api/v1/v1";
 
 app.disable("x-powered-by");
-app.use(apiCors);
-
-app.use(logger);
-app.use(apiValidation);
-app.get("/headers", (req, res, next) => res.json(req.headers));
-app.use("/static", express.static(path.resolve("./", "pubilc", "img")));
-app.get("/", (req, res, next) => {
-    res.send("Tour Booking API.....");
-});
-app.use("/users",userRouter);
-app.use("/tours",toursRouter);
-app.get("/static/download/:id", apiDownloadImage);
-app.use(apiErrorHandler);
+app.use("/v1",routerV1);
 app.listen(process.env.PORT || 5039, () => { console.log("Server Started.......") });
